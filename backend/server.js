ec27242e58572527f4eb4f33f53e5304675e8a62
Chi Quan Luu <samluu206@users.noreply.github.com>
2025-06-10 03:27:58 +1000

    project setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// setup middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
