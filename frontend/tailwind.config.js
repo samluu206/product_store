ce98c3614ea7d7fb759df85799ba87a32d58d027
Chi Quan Luu <samluu206@users.noreply.github.com>
2025-06-21 02:40:21 +1000

    setup navbar and theme selecter

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,27 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    daisyui,
-  ],
-}
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "pastel",
+      "retro",
+      "coffee",
+      "forest",
+      "cyberpunk",
+      "synthwave",
+      "luxury",
+      "autumn",
+      "valentine",
+      "aqua",
+      "business",
+      "night",
+      "dracula",
+    ],
+  },
+};
