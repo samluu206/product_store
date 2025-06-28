9cff6a64baadb1c5a4a1522173454a6059b5260c
Chi Quan Luu <samluu206@users.noreply.github.com>
2025-06-28 19:24:51 +1000

    update navbar and title name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                   className="font-semibold font-mono tracking-widest text-2xl 
                               bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
                 >
-                  POSGRESTORE
+                  LCQ Store
                 </span>
               </div>
             </Link>
